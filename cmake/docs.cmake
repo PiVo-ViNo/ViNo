2e2559fdd43b6eb3050277e64f3bcdc32acb861e
Gleb Gudkov <LineGM@yandex.ru>
2024-03-03 19:50:26 +0300

    add working docs CMake target

--- cmake/docs.cmake
+++ cmake/docs.cmake
@@ -5,12 +5,11 @@ if(DOXYGEN_FOUND)
 	# ---- Объявяем цель для создания документации ----
 	set(DOXYGEN_IN ${PROJECT_SOURCE_DIR}/docs/Doxyfile.in)
 	set(DOXYGEN_OUT ${PROJECT_SOURCE_DIR}/docs/Doxyfile)
+	configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
 
 	add_custom_target(
 		docs
-		configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
 		COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-		COMMAND rm ${DOXYGEN_OUT}
 		COMMENT "Building documentation using Doxygen."
 		WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/docs")
 
