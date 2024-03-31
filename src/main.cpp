ec639f0777557c27cea5af6eb986d7b6d5e6b175
SibJusha <andreas.yushac@gmail.com>
2024-03-31 15:10:38 +0700

    Added basic functionality in main

--- src/main.cpp
+++ src/main.cpp
@@ -10,49 +10,103 @@
 #include <bitset>
 #include <cstring>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <ostream>
 
-const char *_f_path = "--path";
-const char *_f_;
+#include "Parser.h"
+#include "SemanticAnalyzer.h"
+#include "SymbolTable.h"
+#include "TokenEnum.h"
+#include "TokenScanner.h"
+#include "custom_errors.h"
 
-bool path_integrity(char *path) {
+namespace fs = std::filesystem;
+
+namespace m_exe {
+
+const char *arg_path = "--path";
+const char *short_arg_path = "-p";
+
+struct ArgsFlags {
+    bool script_path_set;
+    bool dev_mod_set;
+    bool test_set;
+};
+
+bool path_integrity(char *path) noexcept
+{
     namespace fs = std::filesystem;
 
-    fs::path _src_path{path};
-    if (fs::exists(_src_path) && _src_path.extension() == ".vnsf") return true;
-
+    fs::path src_path(path);
+    if (fs::exists(src_path) && src_path.extension() == ".vnsf") {
+        return true;
+    }
     return false;
 }
 
-int main(int argc, char **argv) {
-    /* Flags for existence of arguments
-     *  0:  --path      / -p
-     *  1:  --dev-mode  / -d
-     *  2:  --test      / -t
-     */
-    std::bitset<3> _flags{};
-
-    std::filesystem::path script_path{};
-
+fs::path check_args(int argc, char **argv, ArgsFlags &args_flags)
+{
+    fs::path script_path;
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
-            std::cout << argv[i] << '\n';
-            if (argc - i >= 2 && !std::strcmp(argv[i], _f_path) &&
-                path_integrity(argv[i + 1])) {
-                _flags.set(0);
-                script_path = argv[i + 1];
-            } else if (false) {
+            if (argc - i >= 1
+                && !(std::strcmp(argv[i], m_exe::arg_path)
+                     && std::strcmp(argv[i], m_exe::short_arg_path)))
+            {
+                if (!m_exe::path_integrity(argv[++i])) {
+                    throw vino::ArgsError("ERROR: Script file error in --path");
+                }
+                args_flags.script_path_set = true;
+                script_path = argv[i];
             } else {
-                std::cout << "ERROR : Unknown argument " << argv[i]
-                          << std::endl;
-                return -1;
+                throw vino::ArgsError("ERROR: Unknown argument: ");
             }
         }
     } else {  // needs a check if there is the running instance of program,
               //  if there are many, give error "Many instances : cannot attach
               //  to random"
-        std::cout << "Program needs a source script file to run" << std::endl;
+        throw vino::ArgsError("Program needs a source script file to run");
+    }
+    return script_path;
+}
+
+}  // namespace m_exe
+
+int main(int argc, char **argv)
+{
+    /* Flags for existence of arguments
+     *  0:  --path      / -p
+     *  1:  --dev-mode  / -d
+     *  2:  --test      / -t
+     */
+    m_exe::ArgsFlags      main_args;
+    std::filesystem::path script_path;
+
+    try {
+        script_path = m_exe::check_args(argc, argv, main_args);
+    } catch (vino::ArgsError &err) {
+        std::cout << err.what() << std::endl;
         return 1;
     }
+
+    using vpair = vino::PairTokenId;
+
+    std::ifstream main_script{script_path, std::ios::binary};
+
+    vino::TokenScanner main_tok_scanner(std::move(main_script));
+
+    std::function<vpair()> f_get_tokens = [&main_tok_scanner] {
+        return main_tok_scanner.get_token();
+    };
+    vino::Parser    main_parser(f_get_tokens);
+    vino::ScriptAst main_ast(main_parser.run());
+    std::cout << "Successful parsing\n" << std::flush;
+
+    vino::SymbolTableEnv   main_symb_env;
+    vino::SemanticAnalyzer main_sem_anal(main_symb_env, main_ast);
+    main_sem_anal.run_analysis();
+    std::cout << "Successful semantic analysis\n" << std::flush;
+
     return 0;
 }
